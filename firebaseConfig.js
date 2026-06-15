a2e8746d88805dba98c71e9a9693dbb2c9bf222b
Micheline922 <michelinekalehezo@gmail.com>
2026-06-15 11:59:04 +0200

    Commit 1

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjwSvwWWbGQKv3Jw3P3VPlChJH2AcxFU0",
+  authDomain: "maman-theodule.firebaseapp.com",
+  projectId: "maman-theodule",
+  storageBucket: "maman-theodule.firebasestorage.app",
+  messagingSenderId: "54718870282",
+  appId: "1:54718870282:web:8801a71e72aadecc7ec0f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
